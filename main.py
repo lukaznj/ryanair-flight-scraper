58633609c2f229dfa87832f090e8a6920edb52cf
Luka Ražem <51300405+Lerdex@users.noreply.github.com>
2024-10-10 19:05:10 +0200

    scrape_engine now scrapes and parses flight data

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from dotenv import load_dotenv
 
-from scrape_engine import scrape_webpage
+from scrape_engine import scrape_flight_list, format_scraped_flights_data
 
 load_dotenv()
 
 if __name__ == "__main__":
-    scrape_webpage(
-        "https://www.ryanair.com/hr/en/trip/flights/select?adults=1&teens=0&children=0&infants=0&dateOut=2024-10-31&dateIn=2024-11-06&isConnectedFlight=false&discount=0&promoCode=&isReturn=true&originIata=ZAG&destinationIata=STN&tpAdults=1&tpTeens=0&tpChildren=0&tpInfants=0&tpStartDate=2024-10-31&tpEndDate=2024-11-06&tpDiscount=0&tpPromoCode=&tpOriginIata=ZAG&tpDestinationIata=STN",
-        "/html/body/app-root/flights-root/div/div/div/div/flights-lazy-content/flights-summary-container/flights-summary/div/div[1]/journey-container/journey/div/div[2]/div/carousel-container/carousel/div/ul/li[3]/carousel-item/button/div[2]/flights-price/ry-price/span[2]")
+    result = scrape_flight_list(
+        "https://www.ryanair.com/hr/en/trip/flights/select?adults=1&teens=0&children=0&infants=0&dateOut=2024-11-03&dateIn=&isConnectedFlight=false&discount=0&promoCode=&isReturn=false&originIata=ZAG&destinationIata=STN&tpAdults=1&tpTeens=0&tpChildren=0&tpInfants=0&tpStartDate=2024-11-03&tpEndDate=&tpDiscount=0&tpPromoCode=&tpOriginIata=ZAG&tpDestinationIata=STN")
+    print(format_scraped_flights_data(result))
--- scrape_engine.py
+++ scrape_engine.py
@@ -1,30 +1,75 @@
 import logging
 import os
+from datetime import datetime
 
 from selenium import webdriver
 from selenium.common import TimeoutException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
 
+from custom_types import FlightRoute, Flight, PriceRecord
 
-def scrape_webpage(url: str, xpath: str) -> None:
+FLIGHT_LIST_XPATH = "/html/body/app-root/flights-root/div/div/div/div/flights-lazy-content/flights-summary-container/flights-summary/div/div[1]/journey-container/journey/flight-list/ry-spinner/div/flight-card-new"
+
+
+def scrape_flight_list(url: str) -> [str]:
+    """
+    Scrapes the flight list from the given URL.
+
+    Args:
+        url (str): The URL to scrape the flight list from.
+
+    Returns:
+        list of str: A list of strings, each representing a flight's details.
+    """
     service = Service(executable_path=os.getenv("CHROMEDRIVER_PATH"))
-
+    # Add Chrome option --headless to run Chrome in headless mode
     driver = webdriver.Chrome(service=service)
 
     try:
         driver.get(url)
 
-        price = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH,
-                                            xpath))
+        elements = WebDriverWait(driver, float(os.getenv("WEBDRIVER_TIMEOUT"))).until(
+            ec.presence_of_all_elements_located((By.XPATH, FLIGHT_LIST_XPATH))
         )
-        print(price.text)
+        return [element.text for element in elements]
 
     except TimeoutException as exception:
-        logging.error("Could not find the element with XPath: %s", xpath, exc_info=exception)
+        logging.error("Could not find the specified element with XPath: %s", FLIGHT_LIST_XPATH, exc_info=exception)
 
     finally:
         driver.quit()
+
+
+def format_scraped_flights_data(flights_data: [str]) -> [FlightRoute]:
+    """
+    Formats the scraped flight data into a list of FlightRoute objects.
+
+    Args:
+        flights_data (list of str): A list of strings, each representing a flight's details.
+
+    Returns:
+        list of FlightRoute: A list of FlightRoute objects containing the formatted flight data.
+    """
+    formated_flights_data = []
+    for flight_data in flights_data:
+        lines = flight_data.split("\n")
+        flight_route = FlightRoute(
+            origin=lines[2],
+            destination=lines[6],
+            flights=[Flight(
+                departure_time=datetime.strptime(lines[1], "%H:%M").time(),
+                arrival_time=datetime.strptime(lines[5], "%H:%M").time(),
+                price_record=[PriceRecord(
+                    price=float(lines[9][1:]),
+                    currency=lines[9][0],
+                    date_time=datetime.now()
+                )]
+            )]
+
+        )
+        formated_flights_data.append(flight_route)
+
+    return formated_flights_data
