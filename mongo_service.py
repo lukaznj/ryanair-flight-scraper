3058c19c59b630afeb8c0d799504a063a6bb4d76
Luka Ražem <51300405+Lerdex@users.noreply.github.com>
2024-10-12 01:07:43 +0200

    Refactored a lot of the code: each type is now saved into the Mongo database as a separate document in a separate collection

--- mongo_service.py
+++ mongo_service.py
@@ -1,6 +1,7 @@
+from bson import ObjectId
 from pymongo import MongoClient
 
-from custom_types import FlightRoute
+from custom_types import FlightRoute, PriceRecord, Flight
 
 
 class MongoService:
@@ -14,32 +15,42 @@ class MongoService:
     def close_connection(self):
         self.client.close()
 
+    def save_price_record(self, price_record: PriceRecord) -> ObjectId:
+        collection = self.get_collection("price_records")
+        result = collection.insert_one(serialize_price_record(price_record))
+        return result.inserted_id
 
-def serialize_new_flight_route(flight_route: FlightRoute):
-    return {
-        "origin": flight_route.origin,
-        "destination": flight_route.destination,
-        "flights": [
-            {
-                "flight_number": flight.flight_number,
-                "departure_time": flight.departure_time.strftime("%H:%M"),
-                "arrival_time": flight.arrival_time.strftime("%H:%M"),
-                "price_record": [
-                    {
-                        "price": price_record.price,
-                        "currency": price_record.currency,
-                        "date_time": price_record.date_time.strftime("%Y-%m-%d %H:%M:%S")
-                    } for price_record in flight.price_record
-                ]
-            } for flight in flight_route.flights
-        ]
+    def save_flight(self, flight: Flight) -> ObjectId:
+        collection = self.get_collection("flights")
+        result = collection.insert_one(serialize_flight(flight))
+        return result.inserted_id
 
-    }
+    def save_flight_route(self, flight_route: FlightRoute) -> ObjectId:
+        collection = self.get_collection("flight_routes")
+        result = collection.insert_one(serialize_flight_route(flight_route))
+        return result.inserted_id
 
 
-def serialize_new_price_record(price_record):
+def serialize_price_record(price_record: PriceRecord):
     return {
         "price": price_record.price,
         "currency": price_record.currency,
         "date_time": price_record.date_time.strftime("%Y-%m-%d %H:%M:%S")
+    }
+
+
+def serialize_flight(flight: Flight):
+    return {
+        "flight_number": flight.flight_number,
+        "departure_time": flight.departure_time.strftime("%H:%M"),
+        "arrival_time": flight.arrival_time.strftime("%H:%M"),
+        "price_record_ids": flight.price_record_ids
+    }
+
+
+def serialize_flight_route(flight_route: FlightRoute):
+    return {
+        "origin": flight_route.origin,
+        "destination": flight_route.destination,
+        "flight_ids": flight_route.flight_ids
     }
