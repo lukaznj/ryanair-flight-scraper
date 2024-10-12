c5650823302dc8ed99a0eb49d9ddf1f097c9baec
Luka Ražem <51300405+Lerdex@users.noreply.github.com>
2024-10-12 21:27:25 +0200

    Optimised the ChromeDriver to open only once per RUN_INTERVAL

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ mongo_service = MongoService(os.getenv("MONGO_URI"), os.getenv("MONGO_DB_NAME"))
 
 if __name__ == "__main__":
     schedule.every(int(os.getenv("RUN_INTERVAL"))).minutes.do(run, mongo_service)
-    # schedule.every(30).seconds.do(run, mongo_service)
+
     while True:
         schedule.run_pending()
         time.sleep(1)
--- run.py
+++ run.py
@@ -6,13 +6,15 @@ from scrape_engine import scrape_flights, get_scraped_flight_number, parse_price
 def run(mongo_service: MongoService):
     print("Scraping flights...")
     updated_flights = []
+    flight_route_urls = []
     flight_routes_collection = mongo_service.get_collection("flight_routes")
 
     for flight_route in flight_routes_collection.find():
-        flight_route_url = flight_route["scrape_url"]
-        scraped_flights = scrape_flights(flight_route_url)
+        flight_route_urls.append(flight_route["scrape_url"])
 
-        for scraped_flight_lines in scraped_flights:
+    scraped_flights = scrape_flights(flight_route_urls)
+    for scraped_flight in scraped_flights:
+        for scraped_flight_lines in scraped_flight:
             updated_flights.append(update_flight(scraped_flight_lines, mongo_service))
 
     print("Updated flights: " + str(updated_flights))
